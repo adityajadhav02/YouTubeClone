7dffab230f861e7e4ea271d97548db21f400667b
Aditya Jadhav <jadhavaditya517@gmail.com>
2023-05-22 13:06:57 +0530

    base ui

---
+++ src/pages/Video.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: "black";
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: "#606060";
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: "black";
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: "#f5f5f5";
+`;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: "black";
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: "#606060";
+  font-size: 12px;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+`;
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
+const Video = () => {
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>7,948,154 views • Jun 22, 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> 123
+            </Button>
+            <Button>
+              <ThumbDownOffAltOutlinedIcon /> Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="/img/noProfile.png" />
+            <ChannelDetail>
+              <ChannelName>YouTube</ChannelName>
+              <ChannelCounter>20M subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloribus laborum delectus unde quaerat dolore culpa sit aliquam
+                at. Vitae facere ipsum totam ratione exercitationem. Suscipit
+                animi accusantium dolores ipsam ut.
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
+        <Hr />
+      </Content>
+    </Container>
+  );
+};
+
+export default Video;
